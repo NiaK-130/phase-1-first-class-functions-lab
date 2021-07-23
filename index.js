fb4897a1e2bb816181a4fcd7ede72dea4e3caffc
Nia K <nsjkappen@gmail.com>
2021-07-23 10:23:18 -0700

    commit tree 3732c08c81299bc1b87a9b3837e2078d286a29a9

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+
+
+    const drivers = ["Sally", "Bob", "Chandler"];
+
+    const returnFirstTwoDrivers = function(drivers){
+        return drivers.slice(0,2)
+    }
+
+
+    const returnLastTwoDrivers = function(drivers){
+        return drivers.slice(-2)
+    }
+    
+
+    const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+
+    const createFareMultiplier = function (FareMultiplier) {
+        return function(value){
+
+            let x = FareMultiplier*value
+
+            return x;
+
+        }
+    }
+
+    const fareDoubler = createFareMultiplier(2);
+    const fareTripler = createFareMultiplier(3);
+
+    function selectDifferentDrivers(drivers, selectingDrivers){
+        return selectingDrivers(drivers);
+    }
